b9f766c899ed3fd5d2920f6ce16806f713962387
parvandobrev <parvan.dobrev@gmail.com>
2020-07-27 10:54:47 +0300

    Update PagingParamsParser.java

--- scimono-server/src/main/java/com/sap/scimono/api/PagingParamsParser.java
+++ scimono-server/src/main/java/com/sap/scimono/api/PagingParamsParser.java
@@ -9,29 +9,29 @@ public class PagingParamsParser {
     super();
   }
 
-  public static int parseCount(String count, int countNum) {
+  public static int parseCount(String countParam, int count) {
     try {
-      countNum = Integer.parseInt(count);
+      count = Integer.parseInt(countParam);
 
-      if (countNum < 0) {
-        countNum = 0;
+      if (count < 0) {
+        count = 0;
       }
     } catch (NumberFormatException e) {
       throw new InvalidInputException("Count is not a numeric value or is out of range.");
     }
-    return countNum;
+    return count;
   }
 
-  public static int parseStartIndex(String startIndex, int startIndexNum) {
+  public static int parseStartIndex(String startIndexParam, int startIndex) {
     try {
-      startIndexNum = Integer.parseInt(startIndex);
+      startIndex = Integer.parseInt(startIndexParam);
 
-      if (startIndexNum < 1) {
-        startIndexNum = 1;
+      if (startIndex < 1) {
+        startIndex = 1;
       }
     } catch (NumberFormatException e) {
       throw new InvalidInputException("StartIndex is not a numeric value or is out of range.");
     }
-    return startIndexNum;
+    return startIndex;
   }
 }
