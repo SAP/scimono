1fc4790a8be49c4dc5a11aa68876917b91679249
Kaiyao Ke <kaiyaok2@illinois.edu>
2022-03-21 22:30:05 +0800

    fix indeterminate orderings

--- scimono-client/src/test/java/com/sap/scimono/client/ResourcePageQueryTest.java
+++ scimono-client/src/test/java/com/sap/scimono/client/ResourcePageQueryTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ResourcePageQueryTest {
   private static final String DEFAULT_URL = "http://localhost:7070/idds/scim/v2/Users";
@@ -21,15 +21,13 @@ public class ResourcePageQueryTest {
   @Test
   public void testCreateDefaultIndexFilter() {
     String resultRequestPath = ResourcePageQuery.indexPageQuery().apply(webTarget).getUri().toString();
-    assertTrue((DEFAULT_URL + "?startIndex=1&count=100").equals(resultRequestPath)
-                || (DEFAULT_URL + "?count=100&startIndex=1").equals(resultRequestPath));
+    assertEquals(DEFAULT_URL + "?startIndex=1&count=100", resultRequestPath);
   }
 
   @Test
   public void testCreateDefaultIdentityFilter() {
     String resultRequestPath = ResourcePageQuery.identityPageQuery().apply(webTarget).getUri().toString();
-    assertTrue((DEFAULT_URL + "?startId=00000000-0000-1000-9000-000000000000&count=100").equals(resultRequestPath)
-                || (DEFAULT_URL + "?count=100&startId=00000000-0000-1000-9000-000000000000").equals(resultRequestPath));
+    assertEquals(DEFAULT_URL + "?startId=00000000-0000-1000-9000-000000000000&count=100", resultRequestPath);
   }
 
   @Test
@@ -38,8 +36,7 @@ public class ResourcePageQueryTest {
     int count = 50;
 
     String resultRequestPath = ResourcePageQuery.indexPageQuery().withStartIndex(startIndex).withCount(count).apply(webTarget).getUri().toString();
-    assertTrue((String.format("%s?startIndex=%d&count=%d", DEFAULT_URL, startIndex, count)).equals(resultRequestPath)
-                || (String.format("%s?count=%d&startIndex=%d", DEFAULT_URL, count, startIndex)).equals(resultRequestPath));
+    assertEquals(String.format("%s?startIndex=%d&count=%d", DEFAULT_URL, startIndex, count), resultRequestPath);
   }
 
   @Test
@@ -48,7 +45,6 @@ public class ResourcePageQueryTest {
     int count = 50;
 
     String resultRequestPath = ResourcePageQuery.identityPageQuery().withStartId(startId).withCount(count).apply(webTarget).getUri().toString();
-    assertTrue((String.format("%s?startId=%s&count=%d", DEFAULT_URL, startId, count)).equals(resultRequestPath)
-                || (String.format("%s?count=%d&startId=%s", DEFAULT_URL, count, startId)).equals(resultRequestPath));
+    assertEquals(String.format("%s?startId=%s&count=%d", DEFAULT_URL, startId, count), resultRequestPath);
   }
 }
