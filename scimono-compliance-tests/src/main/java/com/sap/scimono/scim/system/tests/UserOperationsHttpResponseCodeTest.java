337d1f9a73725f8b830dcfc99ca6192d923fe865
Taras Sologub <taras.sologub@sap.com>
2023-03-30 13:23:50 +0200

    Remove redundant `POST` request when creating user

--- scimono-compliance-tests/src/main/java/com/sap/scimono/scim/system/tests/UserOperationsHttpResponseCodeTest.java
+++ scimono-compliance-tests/src/main/java/com/sap/scimono/scim/system/tests/UserOperationsHttpResponseCodeTest.java
@@ -47,7 +47,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UserOperationsHttpResponseCodeTest extends SCIMHttpResponseCodeTest {
   private static final Logger logger = LoggerFactory.getLogger(UserOperationsHttpResponseCodeTest.class);
 
-  @RegisterExtension 
+  @RegisterExtension
   UserClientScimResponseExtension resourceAwareUserRequest = UserClientScimResponseExtension.forClearingAfterEachExecutions(userRequest);
   
   private final UserFailSafeClient userFailSafeClient = resourceAwareUserRequest.getFailSafeClient();
@@ -69,10 +69,8 @@ public class UserOperationsHttpResponseCodeTest extends SCIMHttpResponseCodeTest
   @DisplayName("Test Create user without userName and verify Http status code: 400")
   @EnableOnUsersBackendState(state = EMPTY)
   public void testCreateUserWithoutUserName400() {
-    final User userToCreate = userFailSafeClient.create(new User.Builder().setDisplayName("testCreatUserWithoutUserName400").build());
-
     logger.info("Creating User without userName");
-    final SCIMResponse<User> scimResponse = resourceAwareUserRequest.createUser(userToCreate);
+    final SCIMResponse<User> scimResponse = resourceAwareUserRequest.createUser(new User.Builder().setDisplayName("testCreatUserWithoutUserName400").build());
     assertAll("Verify Create User Response", getResponseStatusAssertions(scimResponse, false, BAD_REQUEST));
   }
 
